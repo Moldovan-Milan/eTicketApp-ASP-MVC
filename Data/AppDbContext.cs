d1efd54109f3fee44bb6ba630eb872569abb41e2
Moldovan-Milan <moldovanmilan2005@gmail.com>
2024-10-02 17:46:09 +0200

    Created Models and Database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using eTicketApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicketApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string conn = "Server=localhost;Database=etickets;Uid=root;Pwd=;";
+
+        #region Tables
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(conn, ServerVersion.AutoDetect(conn));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m => m.Movie)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(m => m.Actor)
+                .WithMany(am => am.Actors_Movies)
+                .HasForeignKey(m => m.ActorId);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using eTicketApp.Data;
+
 namespace eTicketApp
 {
     public class Program
@@ -8,6 +10,7 @@ namespace eTicketApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>();
 
             var app = builder.Build();
 
